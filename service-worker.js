060e40e195493ec1d71b23eff1a984d726c6e7c3
Andrewcrymble <andrew@crymbleandsons.com>
2026-09-19 23:26:37 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,9 @@
-const CACHE='plaque-designer-v32-ipad-fix';
+const CACHE='plaque-designer-v33-exact-svg';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.mode === 'navigate' || new URL(req.url).pathname.endsWith('/index.html')) {
-    event.respondWith(
-      fetch(req).then(resp => {
-        const copy=resp.clone();
-        caches.open(CACHE).then(c => c.put(req, copy));
-        return resp;
-      }).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(resp => {
-      const copy=resp.clone();
-      caches.open(CACHE).then(c => c.put(req, copy));
-      return resp;
-    }))
-  );
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){
+   e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match('./index.html')));
+ } else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
